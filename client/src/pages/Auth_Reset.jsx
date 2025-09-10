cb8f4383437a80adaf89f72d70dcadaf7813f171
Brian <72092838+BrianWalczak@users.noreply.github.com>
2025-09-09 23:06:39 -0500

    Finish password reset implementation client-side

--- client/src/pages/Auth_Reset.jsx
+++ client/src/pages/Auth_Reset.jsx
@@ -3,17 +3,34 @@ import { useEffect, useState } from "react";
 function Auth_Reset() {
     const [status, setStatus] = useState(null);
     const [success, setSuccess] = useState(false);
+    const [step, setStep] = useState(1);
 
     const handleSubmit = function (e) {
         e.preventDefault();
+        
+        if (step === 2) {
+            const password = $('#password').val();
+            const confirm_password = $('#confirm_password').val();
 
+            if (password !== confirm_password) {
+                setSuccess(false);
+                return setStatus("Your passwords do not match. Please try again.");
+            }
+        }
+        
         $.ajax({
             url: $(e.target).attr('action'),
             method: $(e.target).attr('method'),
             data: $(e.target).serialize(),
             success: function (response) {
                 setSuccess(true);
-                return setStatus("If an account with that email exists, a password reset link has been sent.");
+
+                if(step === 1) {
+                    setStatus("If an account with that email exists, a password reset link has been sent.");
+                } else if(step === 2) {
+                    setStatus("Your password has been successfully reset. Redirecting you to the login page...");
+                    return setTimeout(() => window.location.href = "/auth/sign_in", 1500);
+                }
             },
             error: function (xhr) {
                 setSuccess(false);
@@ -21,6 +38,13 @@ function Auth_Reset() {
             }
         });
     };
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const token = params.get('token');
+
+        if (token?.trim()) return setStep(2);
+    }, []); // on mount, check for token parameter
 
     return (
         <>
@@ -30,20 +54,47 @@ function Auth_Reset() {
             </header>
 
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 space-y-6 mt-20">
-                <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl mb-2 sm:mb-6">Forgot your password?</h2>
-                <p className="text-center text-lg text-black mb-2 sm:mb-6">Don't worry, it happens. Enter your email address below and we'll send you a link to reset your password.</p>
+                <h2 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl mb-2 sm:mb-6">
+                    {step === 1 && "Forgot your password?"}
+                    {step === 2 && "Enter your new password"}
+                </h2>
+                <p className="text-center text-lg text-black mb-2 sm:mb-6">
+                    {step === 1 && "Don't worry, it happens. Enter your email address below and we'll send you a link to reset your password."}
+                    {step === 2 && "Please enter your new password below. Make sure it's at least 8 characters long."}
+                </p>
 
                 <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-md">
                     <form action="http://localhost:3000/api/auth/reset_password" method="POST" className="space-y-6" onChange={() => setStatus(null)} onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="email" className="block text-lg font-medium text-gray-900">Email address</label>
-                            <div className="mt-2">
-                                <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white px-6 py-3 text-lg text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600" />
+                        {step === 2 && (
+                            <input type="hidden" name="reset_token" value={new URLSearchParams(window.location.search).get('token')} />
+                        )}
+
+                        {step === 1 && (
+                            <div>
+                                <label htmlFor="email" className="block text-lg font-medium text-gray-900">Email address</label>
+                                <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white px-6 py-3 text-lg text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 mt-2" />
                             </div>
-                        </div>
+                        )}
+
+                        {step === 2 && (
+                            <div>
+                                <label htmlFor="password" className="block text-lg font-medium text-gray-900">Password</label>
+                                <input id="password" type="password" name="password" required autoComplete="password" className="block w-full rounded-md bg-white px-6 py-3 text-lg text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 mt-2" />
+                            </div>
+                        )}
+
+                        {step === 2 && (
+                            <div>
+                                <label htmlFor="confirm_password" className="block text-lg font-medium text-gray-900">Confirm Password</label>
+                                <input id="confirm_password" type="password" name="confirm_password" required className="block w-full rounded-md bg-white px-6 py-3 text-lg text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-red-600 mt-2" />
+                            </div>
+                        )}
 
                         <div>
-                            <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-6 py-3 text-lg font-bold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 cursor-pointer">Send confirmation email</button>
+                            <button type="submit" className="flex w-full justify-center rounded-md bg-red-600 px-6 py-3 text-lg font-bold text-white shadow-xs hover:bg-red-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600 cursor-pointer">
+                                {step === 1 && "Send Reset Link"}
+                                {step === 2 && "Reset Password"}
+                            </button>
                         </div>
                     </form>
 

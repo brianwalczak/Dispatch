b6151c6827b5a652d81e8be47b8e91ed6af70c8d
Brian <72092838+BrianWalczak@users.noreply.github.com>
2025-09-12 22:26:33 -0500

    Create Dropdown.jsx

---
+++ client/src/components/Dropdown.jsx
@@ -0,0 +1,48 @@
+import { useState, useRef, useEffect } from "react";
+
+function Dropdown({ children, type, className, position = 'left-[52px]' }) {
+    const [open, setOpen] = useState(false);
+    const btn = useRef();
+    const menu = useRef();
+
+    // Find .button and .menu elements from children
+    let buttonChild = null;
+    let menuChild = null;
+
+    (Array.isArray(children) ? children : [children]).forEach(child => {
+        if (child?.props?.className?.includes("button")) buttonChild = child;
+        if (child?.props?.className?.includes("menu")) menuChild = child;
+    });
+
+    // Close dropdown if clicking outside
+    useEffect(() => {
+        const outClick = (event) => {
+            if (menu.current && !menu.current.contains(event.target) && btn.current && !btn.current.contains(event.target)) {
+                setOpen(false);
+            }
+        };
+
+        document.addEventListener("mousedown", outClick);
+        return () => document.removeEventListener("mousedown", outClick);
+    }, []);
+
+    return (
+        <>
+            <div ref={btn} type={type} onClick={() => setOpen(prev => !prev)} className={className}>{buttonChild}</div>
+
+            {open && (
+                <div ref={menu} type={type} menu="true" className={`absolute w-52 bg-white shadow-xl rounded-xl z-50 flex flex-col p-2 ${position}`}>
+                    {menuChild}
+                    <button type="button" className="hidden menu-close" onClick={() => setOpen(false)} />
+                </div>
+            )}
+        </>
+    );
+}
+
+function closeMenu(type) {
+    const menu = document.querySelector(`div[type="${type}"][menu="true"]`);
+    menu?.querySelector("button.menu-close")?.click();
+}
+
+export { Dropdown, closeMenu };

a4ad0d0a68ae21b8f3d6e80bd889b619047aa275
Brian <72092838+BrianWalczak@users.noreply.github.com>
2025-09-24 21:14:16 -0500

    Delete all messages for a session when a session is deleted

---
+++ migrations/20250925020043_cascade_delete_messages/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Message" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "sessionId" TEXT NOT NULL,
+    "senderId" TEXT,
+    CONSTRAINT "Message_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "Session" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Message_senderId_fkey" FOREIGN KEY ("senderId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+INSERT INTO "new_Message" ("content", "createdAt", "id", "read", "senderId", "sessionId") SELECT "content", "createdAt", "id", "read", "senderId", "sessionId" FROM "Message";
+DROP TABLE "Message";
+ALTER TABLE "new_Message" RENAME TO "Message";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;

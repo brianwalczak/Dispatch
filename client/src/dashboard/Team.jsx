ad04d4d83ffdbacbd0902cd8935bda3fe4cb4939
Brian <72092838+BrianWalczak@users.noreply.github.com>
2025-11-24 11:06:21 -0600

    Update popups from Summer of Making to Siege.

--- client/src/dashboard/Team.jsx
+++ client/src/dashboard/Team.jsx
@@ -89,7 +89,7 @@ function Team({ user, onLoad, setToast }) {
                     </p>
                 </div>
 
-                <button onClick={() => alert("Hello there! I see you came from Summer of Making. Well, I'm unfortunately still working on this functionality and it's not quite ready yet (I didn't have enough time to finish by the end of the month).")} className="cursor-pointer px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg text-sm font-medium transition flex items-center gap-2">
+                <button onClick={() => alert("Hello there! I see you came from Siege. Well, I'm unfortunately still working on this functionality and it's not quite ready yet (I didn't have enough time to finish by the end of the month).")} className="cursor-pointer px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-lg text-sm font-medium transition flex items-center gap-2">
                     <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                     </svg>
--- static/src/components/Footer.jsx
+++ static/src/components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="bg-red-500/60 py-5">
             <div className="text-center text-white text-medium">
-                <p><b>&copy; {new Date().getFullYear()} Dispatch. All rights reserved.</b> | Made with <span className="text-red-500">&hearts;</span> for <a href="https://summer.hackclub.com" className="underline">Summer of Making</a>.</p>
+                <p><b>&copy; {new Date().getFullYear()} Dispatch. All rights reserved.</b> | Made with <span className="text-red-500">&hearts;</span> by <a href="https://brianwalczak.com" className="underline">Brian Walczak</a>.</p>
 
                 <p>Open source under Apache 2.0. View on <a href="https://github.com/brianwalczak/dispatch" className="underline" target="_blank" rel="noopener noreferrer">GitHub</a>.</p>
                 </div>
